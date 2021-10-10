685abf0794c6315011418e347155a2d7260f75d5
Joan Rieu <joan@joanrieu.com>
2021-10-10 13:12:51 -0400

    remove pdf resume

--- src/pug.config.js
+++ src/pug.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   locals: {
     links: [
-      ["Resume (PDF)", "/Joan Rieu - Resume.pdf"],
       ["LinkedIn", "https://www.linkedin.com/in/joanrieu/"],
       ["GitHub", "http://github.com/joanrieu"],
       ["Twitter", "https://twitter.com/joanrieu"],
